d7d571838970a5c1b4fb22093455d1a5ec8edbde
Mario Ynocente Castro <ycmario@gmail.com>
2015-12-31 12:05:52 -0500

    Clean costFunction

--- costFunction.py
+++ costFunction.py
@@ -20,9 +20,6 @@ def costFunction(nn_weights, layers, X, y, num_labels, lambd):
     # Unroll Params
     Theta = roll_params(nn_weights, layers)
 
-    # You need to return the following variables correctly
-    J = 0;
-
     # The vector y passed into the function is a vector of labels
     # containing values from 1..K. You need to map this vector into a
     # binary vector of 1's and 0's to be used with the neural network
@@ -31,18 +28,13 @@ def costFunction(nn_weights, layers, X, y, num_labels, lambd):
     for i in range(0,m):
         yv[ y[i] ][i] = 1
 
-    # In this point calculate the cost of the neural network (feedforward)
+    # calculate the cost of the neural network (feedforward)
     a = X
-    #print "shape yv", yv.shape
-    #print "shape a",a.shape
-    #print "shape Theta 0", Theta[0].shape
-    #print "shape Theta 1", Theta[1].shape
 
     for i in range(0,num_layers - 1):
         a = insert(a,0,values=1,axis=1)
         z = a.dot(transpose(Theta[i]))
         a = sigmoid(z)
-        #print "shape a",a.shape
 
     yv = transpose(yv)
     J = (-yv * log(a) - (1 - yv) * log(1 - a)) / m;
