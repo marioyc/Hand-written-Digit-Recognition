59e92aa9fb9382b043d99b8b986d73335e049357
Mario Ynocente Castro <ycmario@gmail.com>
2015-12-31 17:05:41 -0500

    Add predict

--- predict.py
+++ predict.py
@@ -4,16 +4,25 @@ from sigmoid import sigmoid
 def predict(Theta, X):
     # Takes as input a number of instances and the network learned variables
     # Returns a vector of the predicted labels
-    
+
     # Useful values
     m = X.shape[0]
     num_labels = Theta[-1].shape[0]
     num_layers = len(Theta) + 1
 
-    # ================================ TODO ================================
     # You need to return the following variables correctly
     p = zeros((1,m))
- 
-    
-    return p
 
+    for t in range(0,m):
+        a = X[t]
+        print a
+
+        for i in range(0,num_layers - 1):
+            a = insert(a,0,values=1,axis=0)
+            print Theta[i]
+            z = a.dot(transpose(Theta[i]))
+            a = sigmoid(z)
+
+        p[0][t] = argmax(a)
+
+    return p
